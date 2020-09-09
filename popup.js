e8a0b072c1ed2c4d1d7b240bf311d46c560b3c4f
yoshi0202 <kobayashi020202@gmail.com>
2020-09-09 19:33:36 +0900

    feat: popupのスクリプトをlocalStorageを参照するように修正

--- popup.js
+++ popup.js
@@ -1,17 +1,17 @@
 const promiseArr = [];
-const inputList = document.querySelectorAll("input");
 
 promiseArr.push(getLocalStorageData("pullRequest"));
 promiseArr.push(getLocalStorageData("repositoryNavbar"));
 promiseArr.push(getLocalStorageData("conversationLinks"));
 
 Promise.all(promiseArr).then(function (promiseRes) {
-  for (i in inputList) {
-    console.log(inputList[i].id);
-    console.log(promiseRes[i]);
-    console.log(promiseRes[i][inputList[i].id]);
-    inputList[i].checked = promiseRes[i][inputList[i]];
-    inputList[i].addEventListener("change", function (e) {
+  for (i of promiseRes) {
+    // localStorageから取得したオブジェクトのキーを保存
+    const key = Object.keys(i)[0];
+    const element = document.getElementById(key);
+    element.checked = i[key];
+    element.addEventListener("change", function (e) {
+      // チェンジイベントの内容をbackgroundに送信してlocalStorageの値を更新
       chrome.runtime.sendMessage(
         { method: "setItem", key: e.target.name, value: e.target.checked },
         function (response) {
@@ -23,22 +23,6 @@ Promise.all(promiseArr).then(function (promiseRes) {
     });
   }
 });
-// for (i of inputList) {
-//   chrome.runtime.sendMessage({ method: "getItem", key: i.id }, function (res) {
-//     i.checked = res.data;
-//     i.addEventListener("change", function (e) {
-//       console.log(e);
-//       chrome.runtime.sendMessage(
-//         { method: "setItem", key: e.target.name, value: e.target.checked },
-//         function (response) {
-//           if (response.data) {
-//             console.log(response.data);
-//           }
-//         }
-//       );
-//     });
-//   });
-// }
 
 function getLocalStorageData(key) {
   return new Promise(function (res, rej) {
