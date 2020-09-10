1688fcdca54a84bbf8b240cc0f154d13e9d625f7
yoshi0202 <kobayashi020202@gmail.com>
2020-09-11 08:22:09 +0900

    refactor: popup.jsをクラス化

--- src/popup.js
+++ src/popup.js
@@ -1,23 +1,33 @@
 import { createPromiseArray } from "./assets/util";
 
-const promiseArr = createPromiseArray();
-
-Promise.all(promiseArr).then(function (promiseRes) {
-  for (const i of promiseRes) {
-    /* localStorageから取得したオブジェクトのキーを保存 */
-    const key = Object.keys(i)[0];
-    const element = document.getElementById(key);
-    element.checked = i[key];
-    element.addEventListener("change", function (e) {
-      /* チェンジイベントの内容をbackgroundに送信してlocalStorageの値を更新 */
-      chrome.runtime.sendMessage(
-        { method: "setItem", key: e.target.name, value: e.target.checked },
-        function (response) {
-          if (response.data) {
-            console.log(response.data);
-          }
-        }
-      );
-    });
+class Popup {
+  constructor() {
+    this.promiseArr = createPromiseArray();
   }
-});
+
+  async main() {
+    const promiseRes = await Promise.all(this.promiseArr);
+    for (const i of promiseRes) {
+      /* localStorageから取得したオブジェクトのキーを保存 */
+      const key = Object.keys(i)[0];
+      const element = document.getElementById(key);
+      element.checked = i[key];
+      element.addEventListener("change", function (e) {
+        /* チェンジイベントの内容をbackgroundに送信してlocalStorageの値を更新 */
+        chrome.runtime.sendMessage(
+          { method: "setItem", key: e.target.name, value: e.target.checked },
+          function (response) {
+            if (response.data) {
+              console.log(response.data);
+            }
+          }
+        );
+      });
+    }
+  }
+}
+
+window.onload = function () {
+  const popup = new Popup();
+  popup.main();
+};
